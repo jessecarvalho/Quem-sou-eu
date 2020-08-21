c571d6188ed47d26c9dfaf5f626c410541433c50
jesseCarvalho <jesseelias80@gmail.com>
2020-08-21 14:23:30 -0300

    0.9

--- app/public/script/Controller/SiteController.js
+++ app/public/script/Controller/SiteController.js
@@ -5,14 +5,29 @@ class SiteController {
         this.searchEl = document.querySelector("#search");
         this.findEl = document.querySelector("#find");
         this.startEvents();
+
+
     }
 
     startEvents(){
+        const json = 'characters.json';
+
         document.querySelectorAll(".btn-generate").forEach(btn =>
         btn.addEventListener("click", evt => {
+
+            fetch(json)
+                .then(data => data.json())
+                .then(function (data) {
+                    let json = data["Characters"][Math.floor(Math.random()*data["Characters"].length)];
+                    document.querySelector("#characterName").innerHTML = json["name"];
+                    document.getElementById("img").src = "img/" + json["url"];
+                });
             this.searchEl.style.display = "none";
             this.findEl.style.display = "flex";
         }));
+    }
+
+    newCharacter(data){
 
     }
 }
